bfcb72cb0aa2e201548df7854d8ea88565e6e580
Anthony Grimes <i@raynes.me>
2015-05-04 04:07:52 -0700

    Flesh out JSON version a bit more; New types and such

--- src/test/scala/com/scopely/affqis/JsonResults.scala
+++ src/test/scala/com/scopely/affqis/JsonResults.scala
@@ -2,25 +2,15 @@ package com.scopely.affqis
 
 import java.sql.{ResultSet, ResultSetMetaData}
 import java.sql.Types._
+import java.math.{BigInteger, BigDecimal}
 
 import spray.json._
-import DefaultJsonProtocol._
 
 /**
  * Provides a convenient API to produce JSON from a ResultSet, with type metadata info.
  * @param rs A result set.
  */
-class JsonResults(rs: ResultSet) {
-  val meta: ResultSetMetaData = rs.getMetaData
-  val colCount: Integer = meta.getColumnCount
-
-  /**
-   * A map of column names to their type names.
-   */
-  val types: Map[String, String] = (1 to colCount) map { col: Int =>
-    meta.getColumnName(col) -> meta.getColumnTypeName(col)
-  } toMap
-
+object JsonResults {
   /**
    * Converts an object to a JsValue using the specified function. If obj is null, JsNull is returned.
    * @param obj Some kind of object.
@@ -37,30 +27,37 @@ class JsonResults(rs: ResultSet) {
    * @param colIndex index of a column
    * @return a JsValue
    */
-  def jsonColumnVal(colIndex: Int): JsValue = {
+  def jsonColumnVal(rs: ResultSet, meta: ResultSetMetaData)(colIndex: Int): JsValue = {
     val colType: Int = meta.getColumnType(colIndex)
 
     val jsonCol: JsValue = colType match {
-        case DECIMAL | DOUBLE  | FLOAT => toJsVal(rs.getBigDecimal(colIndex), JsNumber(_: BigDecimal))
-        case BIGINT  | INTEGER | TINYINT => toJsVal(rs.getInt(colIndex), JsNumber(_: Int))
+        case DECIMAL | DOUBLE  | FLOAT | NUMERIC | REAL =>
+          toJsVal(rs.getBigDecimal(colIndex), JsNumber(_: BigDecimal))
+        case BIGINT => toJsVal(rs.getLong(colIndex), JsNumber(_: Long))
+        case INTEGER | TINYINT | SMALLINT =>
+          toJsVal(rs.getInt(colIndex), JsNumber(_: Int))
+
         case BOOLEAN => toJsVal(rs.getBoolean(colIndex), JsBoolean(_: Boolean))
         case _ => toJsVal(rs.getString(colIndex), JsString(_: String))
     }
 
-    jsonCol
+    JsObject(
+      "value" -> jsonCol,
+      "type" -> JsString(meta.getColumnTypeName(colIndex)),
+      "name" -> JsString(meta.getColumnName(colIndex))
+    )
   }
 
   /**
    * Get an iterator over a ResultSet where each value is a JSON string.
    * @return A row -> json iterator.
    */
-  def jsonRows: Iterator[String] = {
+  def apply(rs: ResultSet): Iterator[String] = {
+    val meta: ResultSetMetaData = rs.getMetaData
+    val colCount: Integer = meta.getColumnCount
     val results: ResultSetIterator = new ResultSetIterator(rs)
     results.map { cursor: ResultSet =>
-      JsObject(
-        "rows" -> JsArray((1 to colCount).map(jsonColumnVal): _*),
-        "types" -> types.toJson
-      ).toJson.compactPrint
+      JsArray((1 to colCount).map(jsonColumnVal(rs, meta)): _*).compactPrint
     }
   }
 }
